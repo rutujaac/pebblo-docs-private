fa4480008dd5bb5b3b188c129560435a84bec08f
rutujaac <chaudhari.rutuja99@gmail.com>
2024-02-07 16:06:29 +0530

    deployment config

--- classic/docusaurus.config.ts
+++ classic/docusaurus.config.ts
@@ -6,15 +6,14 @@ const config: Config = {
   tagline: "OpenSource Safe DataLoader for Gen AI applications",
   favicon: "img/pebblo-logo.png",
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://rutujaac.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/pebblo-docs-private/",
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
-
+  organizationName: "rutujaac", // Usually your GitHub org/user name.
+  projectName: "pebblo-docs-private", // Usually your repo name.
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   i18n: {
